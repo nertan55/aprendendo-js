848db1cca287a6089a71d495b4d7ffa58f01a78c
nertan55 <nertan2010@hotmail.com>
2023-01-29 20:11:29 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+alert('rodando de uma pagina exter');
+
+
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+
+document.write('aprendendo com document.write');
+
+//variaveis
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
